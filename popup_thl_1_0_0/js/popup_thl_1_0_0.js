0161d69f158d48cfe95942243c34da85c82f5efd
doanduc2000 <doanminhducvip2000@gmail.com>
2022-07-25 14:51:57 +0700

    update

--- popup_thl_1_0_0/js/popup_thl_1_0_0.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('popup-btn').addEventListener('click', () => {
-    document.getElementById('popup_thl_1_0_0').style.display = 'flex'
-})
-document.getElementById('popup-close').addEventListener('click', () => {
-    document.getElementById('popup_thl_1_0_0').style.display = 'none'
-})
